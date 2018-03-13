1081e8fa8390363f31053b12720b1c5fa5f4d680
fuzhentao <1012560716@qq.com>
2018-03-13 09:59:35 +0800

    第一次上传 numpy学习

---
+++ numpy 学习/numpy 属性.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+    2018年3月13日
+    numpy 常用属性学习
+'''
+import numpy as np
---
+++ numpy学习/numpy 属性.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+    2018年3月13日
+    numpy 常用属性学习
+'''
+import numpy as np
+arr1 = np.array([1,2,3,4])
+print(arr1)
+
