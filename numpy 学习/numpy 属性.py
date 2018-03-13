b6357502cd7db721095e3c790abaef34c661d5dd
fuzhentao <1012560716@qq.com>
2018-03-13 10:34:15 +0800

    numpy 属性

--- numpy 学习/numpy 属性.py
+++ numpy 学习/numpy 属性.py
@@ -5,3 +5,9 @@
     numpy 常用属性学习
 '''
 import numpy as np
+arr1 = np.array([[1,2,3],
+          [4,5,6]])
+print(arr1.ndim)    # 矩阵纬度
+print(arr1.dtype)   # 类型
+print(arr1.shape)   # 矩阵几行几列
+print(arr1.size)    # 矩阵长度
